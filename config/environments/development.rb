e6578cafc82d69a115ea8759c5e6541a3610ec46
eric <sonic009@hotmail.com>
2020-02-28 16:25:51 +0000

    Added AJAX calls and live chat screen updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # If some things don't work in the cloud invironment uncomment one of these lines
+  # config.action_cable.disable_request_forgery_protection = true
+  # config.action_cable.allowed_request_origins = ['https://eric-test-message-me-app.herokuapp.com']
 end
